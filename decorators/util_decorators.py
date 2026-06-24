d80ea0cc5fdff74fd79ec151916f8f9442db160c
Kaiden Wang <kaidenwang0309@gmail.com>
2026-06-24 16:58:02 -0700

    Clear util_decorators.py

--- decorators/util_decorators.py
+++ decorators/util_decorators.py
@@ -1,19 +1,9 @@
 from functools import wraps
 
 def combine(dec1, dec2):
-  def decorator(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-      dec1(dec2(func(*args, **kwargs)))
-    return wrapper
-  return decorator
+  # Add code here
+  raise NotImplementedError
 
 def method_combine(dec1, dec2):
-  def decorator(func):
-    @wraps(func)
-    def wrapper(self, *args, **kwargs):
-      @dec1
-      @dec2
-      func(self, *args, **kwargs)
-    return wrapper
-  return decorator
+  # Add code here
+  raise NotImplementedError
