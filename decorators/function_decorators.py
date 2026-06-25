919c0c11ec1fa5bfc280e195b9a57c17a06f61ce
Kaiden Wang <kaidenwang0309@gmail.com>
2026-06-24 17:42:30 -0700

    Added and implemented repeat_while() and fixed bugs

--- decorators/function_decorators.py
+++ decorators/function_decorators.py
@@ -15,6 +15,15 @@ def repeat_until(condition: bool):
   def decorator(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
+      while not condition:
+        func(*args, **kwargs)
+    return wrapper
+  return decorator
+
+def repeat_while(condition: bool):
+  def decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
       while condition:
         func(*args, **kwargs)
     return wrapper
@@ -39,26 +48,26 @@ def call_if(condition: bool, default_error=None, default_response=None):
       else:
         if default_error and default_response is not None:
           raise ValueError("There cannot be both a default error and a default response.")
-        elif default_response is not None:
-          default_response()
         elif default_error is not None:
           raise default_error
+        elif default_response is not None:
+          default_response()
         else:
           return
     return wrapper
   return decorator
 
-def call_unless(condition, default_response=None, default_error=None):
+def call_unless(condition, default_error=None, default_response=None):
   def decorator(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
       if condition:
         if default_response and default_error is not None:
           raise ValueError
-        elif default_response is not None:
-          default_response()
         elif default_error is not None:
           raise default_error
+        elif default_response is not None:
+          default_response()
         else:
           return
         else:
